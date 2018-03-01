78940027bc6a789720a32645d4cabbd0545fc0d3
Denis <32365426+ProkopenkovIKBO0317@users.noreply.github.com>
2018-03-02 01:12:32 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -3,15 +3,19 @@
 #include <iostream>
 #include <math.h>
 
+
 using namespace std;
 
 void print_error()
 {
+
 	cout << "Error, restart the program!" << endl;
+
 }
 
 void print_menu(int select)
 {
+
 	cout << "Enter operation" << endl;
 	cout << "------------------------" << endl;
 	cout << "1. Sum" << endl;
@@ -20,22 +24,51 @@ void print_menu(int select)
 	cout << "4. Division" << endl;
 	cout << "5. Exponentiation" << endl;
 	cout << "------------------------" << endl;
+
 }
 
-void toString(Complex c)
+Complex print_Complex(Complex c)
 {
-	cout << "Your complex number is (" << c->real << ';' << c->imag << ')' << endl;
+	cout << "Complex number is (" << c->real << ';' << c->imag << ')' << endl;
+
+	return c;
 }
 
-Complex ComplexCreate(Complex c1)
+complex EnterComplex(Complex a, Complex b)
 {
-	cout << "Enter real and imag nubmers" << endl;
-	cin >> c1->real >> c1->imag;
-	return c1;
+	cout << "Enter the first complex number: real/imag" << endl;
+	cin >> a->real >> a->imag;
+
+	cout << "Enter the second complex number: real/imag" << endl;
+	cin >> b->real >> b->imag;
+
+	return *a, *b;
 }
 
-Complex complexSum(Complex c1, Complex c2)
+complex complexSum(Complex a, Complex b)
 {
-	return ComplexCreate(c1->real + c2->real, c1->imag + c2->real);
+	complex c;
+
+	c.real = a->real + b->real;
+	c.imag = a->imag + b->imag;
+
+	return c;
+}
+
+complex complexSubstraction(Complex a, Complex b)
+{
+	complex c;
+
+	c.real = a->real - b->real;
+	c.imag = a->imag - b->imag;
+
+	return c;
+}
+
+double complexMultiply(Complex a, Complex b)
+{
+	double c;
+	c = (a->real * b->real - a->imag * b->imag) + (a->real * b->imag + b->real * a->imag);
+		return c;
 }
  
