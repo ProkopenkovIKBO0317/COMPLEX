a67126c702d6f5402bd9f77a1c86a07db86044a9
Denis <32365426+ProkopenkovIKBO0317@users.noreply.github.com>
2018-03-02 01:12:51 +0300

    Update complex.h

--- complex.h
+++ complex.h
@@ -3,20 +3,23 @@
 #include <cmath>
 #include <string.h>
 
-
 typedef struct complex * Complex;
 
 typedef struct complex
 {
-	int real;
-	int imag;
+	double real;
+	double imag;
 }complex;
 
 void print_error();
-void toString(Complex c);
+
 void print_menu(int select);
-Complex ComplexCreate(Complex c1);
-Complex complexSum(Complex c1, Complex c2);
+
+Complex print_Complex(Complex c);
+complex EnterComplex(Complex a, Complex b);
+complex complexSum(Complex a, Complex b);
+complex complexSubstraction(Complex a, Complex b);
+double complexMultiply(Complex a, Complex b);
 
 
 
