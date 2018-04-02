87f6cf8a1fe94dbf815adc5f8f579cb4728f27be
Denis <32365426+ProkopenkovIKBO0317@users.noreply.github.com>
2018-04-02 15:35:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
 	complex *c1 = new complex; // выделяем память для объектов класса complex.
 	complex *c2 = new complex;
 	complex *c3 = new complex;
-	double mod;
+	double *mod = new double;
 
 
 	print_menu(select); // меню выбора операций
@@ -21,34 +21,29 @@ int main()
 	switch (select)
 	{
 	case '+': //операция сложение
-		c1->EnterComplex(c1);
-		c2->EnterComplex(c2);
+		c1, c2->EnterComplex(c1, c2);
 		c3->complexSum(c1, c2);
 		break;
-
+		
 	case '-': // операция вычитание
-		c1->EnterComplex(c1);
-		c2->EnterComplex(c2);
+		c1, c2->EnterComplex(c1, c2);
 		c3->complexSubstraction(c1, c2);
 		break;
 
 	case '*': // операция умножение
-		c1->EnterComplex(c1);
-		c2->EnterComplex(c2);
+		c1, c2->EnterComplex(c1, c2);
 		c3->complexMultiply(c1, c2);
 		break;
 
 	case '/': // операция деление
-		c1->EnterComplex(c1);
-		c2->EnterComplex(c2);
+		c1, c2->EnterComplex(c1, c2);
 		c3->complexDivision(c1, c2);
 		break;
 
 	case 'M': // нахождение модуля комплексного числа
 	case 'm':
 		c1->EnterComplex(c1);
-		mod = complexModulo(c1);
-		cout << "Modulo = " << mod << endl;
+		c1->complexModulo(c1);
 		break;
 
 	default: // вызов ошибки в случае введения неверного символа.
@@ -60,6 +55,7 @@ int main()
 	delete c1; // удаляем память для объектов класса так как она является динамической.
 	delete c2;
 	delete c3;
+	delete mod;
 }
 
 
